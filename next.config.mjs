457a5896193c13ebb947b82fdc275431ab6b52ff
MundhadHarsh <harshmundhada@gmail.com>
2025-05-06 02:16:48 +0530

    updated config files

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,3 @@
 const nextConfig = {};
 
 export default nextConfig;
-
-// module.exports = {
-//     env: {
-//         customKey:  'my-value',
-//         SERVICEID:  'service_n90h4im',
-//         TEMPLATEID: 'template_dpgf5a8',
-//         PUBLICKEY : 'jbKbcG1keAfdRWDIZ',
-//     },
-//   }
